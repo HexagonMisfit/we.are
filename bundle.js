0cb5d9ed8873951e218df12e2a780a36a3f8981c
hurricane987 <aelliotterickson@gmail.com>
2018-02-08 22:04:20 -0800

    simplify camera movement function

--- bundle.js
+++ bundle.js
@@ -10516,11 +10516,19 @@ var cubeMesh;
 var cubePositions = [];
 var cubeRotationVelocities = [];
 var cubeScale;
-camera.position.z = 5;
-var mousePos = new THREE.Vector3();
-
+camera.position.z = 6;
+var target = new THREE.Object3D();
+target.position.set(0,0,-15);
 var width = window.innerWidth;
 var height = window.innerHeight;
+var mouseX = 0;
+var mouseY = 0;
+var maxX = 7;
+var maxY = 7;
+var tempX = 0;
+var tempY = 0;
+var halfWidth = width / 2;
+var halfHeight = height / 2;
 var yOffset = 30;
 
 $(document).ready(function () {
@@ -10530,15 +10538,19 @@ $(document).ready(function () {
     // copy initial camera rotation so we can tween from it to a new one in slo mo based on mouse movement
 
     function onMouseMove(event) {
-        var tempX = (event.clientX / width) * 2 - 1;
-        var tempY = -((event.clientY / height) * 2 - 1);
-        TweenMax.to(mousePos, 5, {
-            x: tempX,
-            y: tempY,
-            onUpdate: function () {
-                camera.lookAt(mousePos);
-            }
-        });
+        // var tempX = (event.clientX / width) * 2 - 1;
+        // var tempY = -((event.clientY / height) * 2 - 1);
+        // TweenMax.to(mousePos, 5, {
+        //     x: tempX,
+        //     y: tempY,
+        //     onUpdate: function () {
+        //         camera.lookAt(mousePos);
+        //     }
+        // });
+        mouseX = event.clientX - halfWidth;
+        mouseY = event.clientY - halfHeight;
+        tempX = (mouseX - target.position.x) * 0.00002;
+        tempY = (mouseY - target.position.y) * 0.00002;
     }
 
     function toPositions() {
@@ -10580,23 +10592,31 @@ $(document).ready(function () {
         cube.rotation.x = Math.random();
 
         cubeGroup.add(cube);
-        cubeRotationVelocities.push([ Math.random() / 400, Math.random() / 400 ]);
+        cubeRotationVelocities.push([Math.random() / 400, Math.random() / 400]);
     }
 
     scene.add(cubeGroup);
 
+
     // Animate the scene
+
+
 
     var animate = function () {
         requestAnimationFrame(animate);
         for (var i = 0; i < cubeGroup.children.length; i++) {
             rotate(cubeGroup.children[i], cubeRotationVelocities[i]);
         }
+        if(target.position.x + tempX <= maxX && target.position.x + tempX >= -maxX) {
+            target.position.x += (mouseX - target.position.x) * 0.00002;
+        }
+        if(target.position.y - tempY <= maxY && target.position.y + tempY >= -maxY) {
+            target.position.y -= (mouseY - target.position.y) * 0.00002;
+        }        
+        camera.lookAt(target.position);
+
         renderer.render(scene, camera);
     };
-
-    animate();
-    toPositions();
 
     // Draw it in the dom and add resize event listener
 
@@ -10613,6 +10633,9 @@ $(document).ready(function () {
     }
 
     renderer.domElement.id = "header-canvas";
+
+    animate();
+    toPositions();
 });
 /* WEBPACK VAR INJECTION */}.call(__webpack_exports__, __webpack_require__(0)))
 
