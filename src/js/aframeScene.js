3292a7ccd3026975a07b62b573f1ae459b4c9805
hurricane987 <aelliotterickson@gmail.com>
2018-02-19 23:07:13 -0800

    gentle globelike shader on vr sky

--- src/js/aframeScene.js
+++ src/js/aframeScene.js
@@ -16,26 +16,35 @@ const fragmentShader = `
 
 varying vec2 vUv;
 uniform float time;
+uniform vec3 colorIn;
 
 float plot(vec2 st, float pct){
-    return  smoothstep( pct-0.5, pct, st.y) -
-            smoothstep( pct, pct+0.5, st.y);
+    return  smoothstep( pct-0.2, pct, st.y) -
+            smoothstep( pct, pct+0.2, st.y);
+  }
+
+float plot2(vec2 st, float pct){
+    return  smoothstep( pct-0.1, pct, st.x) -
+            smoothstep( pct, pct+0.1, st.x);
   }
   
   void main()
   {
     vec2 st = vUv.xy;
   
-    float x = fract( (st.y + time * 0.1) * 20.0) * 10.0 - 5.0;    
-    float y = x;
+    float x = fract( (st.y + time * 0.05) * 20.0) * 10.0 - 5.0;    
+    float y = fract(st.x * 20.0) * 10.0 / 2.0 - 3.9;
     float z = x;
-    vec3 color = vec3(y,x,z);
+    vec3 color, color2 = vec3(y,x,z);
 
     // Plot a line
     float pct = plot(st,x);
-    color = (pct)*color+pct*vec3(0.5,0.25,0.4);
-
-	gl_FragColor = vec4(color,1.0);
+    float pct2 = plot2(st, y);
+    color = (pct)*color+pct*vec3(1.0,1.0,1.0);
+    color2 = (pct2)*color+pct2*vec3(1.0,1.0,1.0);
+    color = mix(color, colorIn, abs(sin(time)) * 0.4 + 0.4);
+    color = mix(color, color2, 0.5);
+	gl_FragColor = vec4(vec3(st.y) + (color * (1.0 - st.y)),1.0);
   }
 `;
 
@@ -50,7 +59,7 @@ AFRAME.registerComponent('material-grid-glitch', {
         this.material = new THREE.ShaderMaterial({
             uniforms: {
                 time: { value: 0.0 },
-                color: { value: new THREE.Color(data.color) }
+                colorIn: { value: new THREE.Color(data.color) }
             },
             vertexShader,
             fragmentShader
@@ -62,7 +71,7 @@ AFRAME.registerComponent('material-grid-glitch', {
      * Update the ShaderMaterial when component data changes.
      */
     update: function () {
-        this.material.uniforms.color.value.set(this.data.color);
+        this.material.uniforms.colorIn.value.set(this.data.color);
     },
     /**
      * Apply the material to the current entity.
