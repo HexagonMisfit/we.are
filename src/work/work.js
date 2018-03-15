3402be18064e743e8904e7c04af0124400c99229
hurricane987 <aelliotterickson@gmail.com>
2018-03-14 23:50:15 -0700

    more work on redesign

--- src/work/work.js
+++ src/work/work.js
@@ -17,39 +17,64 @@ import '../sass/main.scss';
 var projects = [
     {
         title: 'Mariposa Chandelier',
-        id: '0',
+        id: '01',
         backgroundColor: brandColors.brightYellow,
         img: './assets/mariposa_1280x720.jpg',
-        blurb: 'ecteturn chendelier cool apartment complex have fun with it. And then taking from his wallet an old schedule of trains he\'ll say "I told you when I came I was a stranger".'
+        blurb: 'Ecteturn chendelier cool apartment complex have fun with it. Gibberish filler content to look real and explain space taken up by blurb element. Life is much better when spent with brothers from other mothers, is what they say.',
+        stats: {
+            "Location": "Denver, CO",
+            "Technologies used": "Unity, Processing, Custom-built LED arrays",
+            "Year": "2014"
+        }
     },
     {
         title: 'Music Visualizer',
-        id: '1',
+        id: '02',
         backgroundColor: brandColors.salmonPink,
         img: './assets/daniel_sierra_project.jpg',
-        blurb: 'Lorchendelier cool apartment complex have fun with it. And then taking from his wallet an old schedule of trains he\'ll say "I told you when I came I was a stranger".'
+        blurb: 'In the champagne room with the models is not really the time or place for ideological battles. And then taking from his wallet an old schedule of trains he\'ll say "I told you when I came I was a stranger".',
+        stats: {
+            "Location": "Anywhere and everywhere",
+            "Technologies used": "Processing, MIDI Controllers",
+            "Year": "Ongoing"
+        }
     },
     {
         title: 'Coachella VIP Party',
-        id: '2',
+        id: '03',
         backgroundColor: brandColors.lavendarIsh,
-        img: './assets/daniel_sierra_project.jpg',
-        blurb: 'Lorem ipsum consecteturn chendelier cool apartment complex have fun with it. And then taking from his wallet an old schedule of trains he\'ll say "I told you when I came I was a stranger".'
+        img: './assets/mariposa_1280x720.jpg',
+        blurb: 'Rock climbing can be a very challenging and rewarding adventure, but the risks are multiplied to the extent that intoxicants are involved. Having fun with technology is a likely outcome of learning to write code.',
+        stats: {
+            "Location": "Indio, CA",
+            "Technologies used": "Xbox Kinect, Processing, Computer vision software",
+            "Year": "2015"
+        }
     },
     {
         title: 'VR Ballet Experience',
-        id: '3',
+        id: '04',
         backgroundColor: brandColors.seafoamGreen,
         img: './assets/daniel_sierra_project.jpg',
-        blurb: 'balle fun is great in love with the modern age, last time we met an old schedule of trains he\'ll say "I told you when I came I was a stranger".'
+        blurb: 'Ballet fun is great in love with the modern age, office of smellography kicks in when you know your time is right to get level-headed and exit stage left. Smoking prohibited in cockpit of spacecraft.',
+        stats: {
+            "Location": "Seattle, WA",
+            "Technologies Used": "Unity, HTC Vive, Cinema 4D, Motion capture",
+            "Year": "2017"
+        }
     },
     {
         title: 'Digital Fish Tank',
-        id: '4',
+        id: '05',
         backgroundColor: brandColors.blueLight,
-        img: './assets/daniel_sierra_project.jpg',
-        blurb: 'Lorem ipsum cons apartment complex have fun with it. And then taking from his wallet an old schedule of trains he\'ll say "I told you when I came I was a stranger".'
-    },
+        img: './assets/mariposa_1280x720.jpg',
+        blurb: 'Lorem ipsum consectetur halogen ipso facto. And then taking from his wallet an old schedule of trains, a big sandwich made of cookie dough deep fried in fat carved from last night\'s roast. Billy Grippo top eastside producer.',
+        stats: {
+            "Location": "Hillsboro, OR",
+            "Technologies Used": "Unity, Computer vision technology",
+            "Year": "2017"
+        }
+    }
 ];
 
 $(document).ready(function () {
@@ -57,25 +82,37 @@ $(document).ready(function () {
 
     var activeProject = projects[0];
 
-    projects.forEach(function(project) {
-        projectLinks.each(function(link) {
+    projects.forEach(function (project) {
+        projectLinks.each(function (link) {
             console.log();
-            if($(projectLinks[link]).attr('data') === project.id) {
+            if ($(projectLinks[link]).attr('data') === project.id) {
                 $(projectLinks[link]).children().text(project.title);
             }
         });
     })
 
     function setActiveProject(id) {
-        activeProject = _.find(projects, { id: id });   
-        $('.work-header-text').text(activeProject.title)
-        $('.project-blurb').fadeOut('fast', function() {
-            $('.project-blurb').text(activeProject.blurb); 
-            $('.project-blurb').fadeIn(); 
+        activeProject = _.find(projects, { id: id });
+        $('.work-header-text').fadeOut('fast', function() {
+            $('.work-header-text').text(activeProject.title);
+            $('.work-header-text').fadeIn();
+        });
+        $('.project-number').text(activeProject.id);
+        $('.project-blurb').fadeOut('fast', function () {
+            $('.project-blurb').text(activeProject.blurb);
+            $('.project-blurb').fadeIn();
         });
         $('#project-img').fadeOut('fast', function () {
             $('#project-img').attr('src', activeProject.img);
             $('#project-img').fadeIn();
+        });
+        $('.project-stats').fadeOut('fast', function () {
+            var keys = Object.keys(activeProject.stats);
+            for (var i = 0; i < keys.length; i++) {
+                $('#stat-' + i.toString() + '-key').text(keys[i]);
+                $('#stat-' + i.toString() + '-value').text(activeProject.stats[keys[i]]);
+            }
+            $('.project-stats').fadeIn();
         });
     }
 
@@ -84,5 +121,5 @@ $(document).ready(function () {
         $(this).addClass('active light-text');
         setActiveProject($(this).attr('data'));
     });
-    setActiveProject('0');
+    setActiveProject('01');
 });
