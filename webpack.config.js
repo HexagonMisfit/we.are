544b51e79817a0e5843d431678b09c1b7eac6b4e
hurricane987 <aelliotterickson@gmail.com>
2018-04-19 22:11:27 -0700

    try publicPath in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,8 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: './js/[name].bundle.js'
+        filename: './js/[name].bundle.js',
+        publicPath: '/hexmisfitsite/'
     },
     module: {
         rules: [
@@ -53,33 +54,33 @@ module.exports = {
         new ExtractTextPlugin('style.css'),
         new HtmlWebpackPlugin({ 
             template: './index.html', 
-            filename: './index.html', 
+            filename: '/index.html', 
             excludeChunks: ['home','team','work','contact','vr'],
             inject: 'head'
         }),
         new HtmlWebpackPlugin({
             template: './team/team.html',
-            filename: './templates/team.html',
+            filename: '/templates/team.html',
             chunks: ['team']
         }),
         new HtmlWebpackPlugin({
             template: './home/home.html',
-            filename: './templates/home.html',
+            filename: '/templates/home.html',
             chunks: ['home']
         }),
         new HtmlWebpackPlugin({
             template: './work/work.html',
-            filename: './templates/work.html',
+            filename: '/templates/work.html',
             chunks: ['work']
         }),
         new HtmlWebpackPlugin({
             template: './vr/vr.html',
-            filename: './vr.html',
+            filename: '/vr.html',
             chunks: ['vr']
         }),
         new HtmlWebpackPlugin({
             template: './contact/contact.html',
-            filename: './templates/contact.html',
+            filename: '/templates/contact.html',
             chunks: ['contact']
         }),
         new webpack.ProvidePlugin({
